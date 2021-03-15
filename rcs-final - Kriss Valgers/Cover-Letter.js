f0dad4695ee908a4e2efd6ee5a851f775f69df2f
Kriss <80677770+KrissValgers@users.noreply.github.com>
2021-03-15 17:34:10 +0200

    Add files via upload

---
+++ rcs-final - Kriss Valgers/Cover-Letter.js
@@ -0,0 +1,15 @@
+button = document.getElementById(".button");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
---
+++ rcs-final - Kriss Valgers/Home.js
@@ -0,0 +1,45 @@
+const text = document.querySelector(".name-title");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+
+let char = 0;
+let timer = setInterval(onTick, 100);
+
+function onTick() {
+    const span = text.querySelectorAll("span")[char];
+    span.classList.add('fade');
+    char++;
+    if (char === splitText.lenght) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
+
+
+button = document.getElementById(".button");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
